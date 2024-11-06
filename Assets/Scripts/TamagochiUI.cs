60d963303a892ac0ac9e1ac7f8514171fa5ff440
FerLS <fernando.alvarez@udc.es>
2024-11-06 10:38:52 +0100

    Basic UI

---
+++ Assets/Scripts/TamagochiUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TamagochiUI : MonoBehaviour
+{
+    [Header("Body Parts")]
+
+    [SerializeField] private Image[] Pupils;
+    [SerializeField] private Image[] Body;
+
+    [Header("Customize")]
+
+    [SerializeField] private Color bodyColor;
+
+    [SerializeField] private Color eyesColor;
+
+
+
+    void SetEyesColor()
+    {
+
+        foreach (var pupil in Pupils)
+        {
+            pupil.color = eyesColor;
+        }
+    }
+
+    void SetBodyColor()
+    {
+
+        foreach (var bodyPart in Body)
+        {
+            bodyPart.color = bodyColor;
+        }
+
+    }
+    private void OnValidate()
+    {
+
+        SetEyesColor();
+        SetBodyColor();
+
+    }
+}
