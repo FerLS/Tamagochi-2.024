73825c4bfd59abbe843d644454de4223abc9c5c2
JosefinaFerrari <josefina.ferrari02@gmail.com>
2024-12-03 19:24:04 +0100

    Add comment to feeling

--- Assets/Scripts/GlobalUi.cs
+++ Assets/Scripts/GlobalUi.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class GlobalUi : MonoBehaviour
 {
@@ -6,11 +7,36 @@ public class GlobalUi : MonoBehaviour
     public GameObject InitialScreen;
     public GameObject GameScreen;
     public GameObject SettingsScreen;
-
     public GameObject ReportsScreen;
+
+    [Header("Inputs")]
+    public TMP_InputField commentInputField; 
+
+    private string selectedEmotion;
+    private string enteredNote;
+
+
+
+    private void Start()
+    {
+        if (commentInputField == null)
+        {
+            commentInputField = GameObject.Find("commentInputField").GetComponent<TMP_InputField>();
+
+            if (commentInputField == null)
+            {
+                Debug.LogError("commentInputField could not be found in the scene!");
+            }
+            else
+            {
+                Debug.Log("commentInputField successfully assigned at runtime.");
+            }
+        }
+    }
+
     public void EnterGameScreen()
     {
-
+        SaveEmotionAndNote();
         InitialScreen.SetActive(false);
         GameScreen.SetActive(true);
     }
@@ -32,5 +58,14 @@ public class GlobalUi : MonoBehaviour
         SettingsScreen.SetActive(false);
     }
 
+    private void SaveEmotionAndNote()
+    {
+        enteredNote = commentInputField.text;
+
+        PlayerPrefs.SetString("EnteredNote", enteredNote);
+        PlayerPrefs.Save();
+
+        Debug.Log($"Comment Added: {enteredNote}");
+    }
 
 }
