8d6783b2390538fbde20f457b93a8c33bf4699c6
JosefinaFerrari <Josefina.ferrari02@gmail.com>
2025-01-03 19:40:08 -0300

    multiple emotions possibility in same date

--- Assets/Scripts/EmotionSelector.cs
+++ Assets/Scripts/EmotionSelector.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 public class EmotionSelector : MonoBehaviour
 {
@@ -14,19 +15,34 @@ public class EmotionSelector : MonoBehaviour
     public void SaveEmotion()
     {
         string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+        string currentTime = DateTime.Now.ToString("HH:mm:ss");
 
         EmotionData emotionData = new EmotionData
         {
             emotion = selectedEmotion,
-            date = currentDate
+            date = currentDate,
+            time = currentTime
         };
 
-
-        string json = JsonUtility.ToJson(emotionData, true);
-
         string filePath = Application.dataPath + $"/SavedData/{currentDate}.json";
-        //string filePath = Application.persistentDataPath + $"/{currentDate}.json";
 
+        List<EmotionData> emotionList = new List<EmotionData>();
+
+        // Si el archivo ya existe, leer los datos existentes
+        if (File.Exists(filePath))
+        {
+            string existingJson = File.ReadAllText(filePath);
+            emotionList = JsonUtility.FromJson<EmotionDataList>(existingJson).emotionDataList;
+        }
+
+        // Agregar el nuevo EmotionData a la lista
+        emotionList.Add(emotionData);
+
+        // Serializar la lista completa de EmotionData
+        EmotionDataList emotionDataList = new EmotionDataList { emotionDataList = emotionList };
+        string json = JsonUtility.ToJson(emotionDataList, true);
+
+        // Guardar el archivo con los datos actualizados
         File.WriteAllText(filePath, json);
 
         Debug.Log($"Saved on File: {filePath}");
@@ -38,4 +54,11 @@ public class EmotionData
 {
     public string emotion;
     public string date;
+    public string time;
+}
+
+[Serializable]
+public class EmotionDataList
+{
+    public List<EmotionData> emotionDataList;
 }
