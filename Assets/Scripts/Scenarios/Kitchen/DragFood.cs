aeaee2b10d5ba35df71e4c70400e8eb0c5dbfd95
Pilar Bocage <bocagepili@gmail.com>
2025-02-10 17:17:35 -0300

    food returning to its place

--- Assets/Scripts/Scenarios/Kitchen/DragFood.cs
+++ Assets/Scripts/Scenarios/Kitchen/DragFood.cs
@@ -5,13 +5,13 @@ using UnityEngine.EventSystems;
 public class DragFood : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
     private RectTransform rectTransform;
-
+    private Vector3 originalPosition; // Almacena la posición original
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        originalPosition = rectTransform.position; // Guarda la posición original al inicio
     }
-
 
     public void OnDrag(PointerEventData eventData)
     {
@@ -21,25 +21,34 @@ public class DragFood : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointer
 
     public void OnPointerUp(PointerEventData eventData)
     {
-
         Debug.Log("OnPointerUp");
         List<RaycastResult> results = new List<RaycastResult>();
         EventSystem.current.RaycastAll(eventData, results);
         Debug.Log(results.Count);
 
+        bool droppedOnTamagochi = false;
+
         foreach (RaycastResult result in results)
         {
             Debug.Log(result.gameObject.name);
-            if (result.gameObject.CompareTag("Tamagochi"))
+            if (result.gameObject.CompareTag("Tamagochi")) // Si se suelta sobre el Tamagotchi
             {
-                Destroy(gameObject);
+                Destroy(gameObject); // El Tamagotchi "come" la comida
+                droppedOnTamagochi = true;
                 break;
             }
+        }
+
+        // Si NO se soltó sobre el Tamagotchi, regresa a la posición original
+        if (!droppedOnTamagochi)
+        {
+            rectTransform.position = originalPosition;
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-
+        // Guarda la posición original nuevamente en caso de que haya sido movida antes
+        originalPosition = rectTransform.position;
     }
 }
