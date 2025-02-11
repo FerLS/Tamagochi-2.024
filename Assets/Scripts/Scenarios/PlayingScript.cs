d6dfbf54cff8588da7cf14999e559d81b0008557
JosefinaFerrari <Josefina.ferrari02@gmail.com>
2025-02-11 00:50:12 -0300

    dissapearing ball when canPlay = false

--- Assets/Scripts/Scenarios/PlayingScript.cs
+++ Assets/Scripts/Scenarios/PlayingScript.cs
@@ -5,10 +5,15 @@ public class PlayingScript : MonoBehaviour
     [Header("Emotion System")]
     [SerializeField] private EmotionSystem emotionSystem;
 
-
     [Header("Values")]
     [SerializeField] private float increaseRate = 8f;
     [SerializeField] private bool canPlay = true;
+
+    [Header("Game Objects to Enable/Disable")]
+    [SerializeField] private GameObject ticTacToe;
+    [SerializeField] private GameObject memory;
+    [SerializeField] private GameObject cupsBall;
+    [SerializeField] private GameObject park;
 
     private float timeElapsed = 0f;
     private int index = 0;
@@ -16,6 +21,7 @@ public class PlayingScript : MonoBehaviour
     void Start()
     {
         validatePlayingRule();
+        UpdateGameObjects();
     }
 
     void Update()
@@ -39,6 +45,8 @@ public class PlayingScript : MonoBehaviour
             }
 
             validatePlayingRule();
+
+            UpdateGameObjects();
         }
     }
     private void IncreaseSleepy()
@@ -64,5 +72,17 @@ public class PlayingScript : MonoBehaviour
     private void validatePlayingRule()
     {
         canPlay = (emotionSystem.GetEnergy() >= 0.1);
+    }
+
+    private void UpdateGameObjects()
+    {
+        if (ticTacToe) ticTacToe.SetActive(canPlay);
+        if (memory) memory.SetActive(canPlay);
+        if (cupsBall) cupsBall.SetActive(canPlay);
+        if (park)
+        {
+            GameObject ball = park.transform.Find("Ball").gameObject;
+            if (ball) ball.SetActive(canPlay);
+        }
     }
 }
